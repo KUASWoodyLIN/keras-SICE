f2dde0ad4e9d343adacdc9babb228b14fc62d7bd
KUASWoodyLin <jj7158761@gmail.com>
2019-03-05 08:19:29 +0000

    fix the luminance netwrok

--- losses/losses_layer.py
+++ losses/losses_layer.py
@@ -14,3 +14,18 @@ class L1Losses(tf.keras.layers.Layer):
         loss = tf.reduce_sum(tf.abs(y_true - y_pred), axis=[1, 2, 3])
         # self.add_loss(loss, inputs=inputs)
         return tf.reduce_mean(loss)
+
+
+class MSELosses(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(MSELosses, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(MSELosses, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        y_pred = inputs[0]
+        y_true = inputs[1]
+        loss = tf.reduce_sum(tf.squared_difference(y_true, y_pred), axis=[1, 2, 3])
+        # self.add_loss(loss, inputs=inputs)
+        return tf.reduce_mean(loss)
--- model/sice.py
+++ model/sice.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-from losses.losses_layer import L1Losses
 from layer.clip import ClipLayer
+from losses.losses_layer import L1Losses, MSELosses
 from losses.losses import pass_loss, dssim_loss, mse_loss
 from layer.filter import GaussianFilter
 from layer.conv import Conv2DTranspose
@@ -72,32 +72,34 @@ def whole_image_enhancement_network(inputs):
 
 def create_train_model():
     inputs = tf.keras.Input(shape=(None, None, 3))
-    inputs_y = tf.keras.Input(shape=(None, None, 3))
+    inputs_y_true = tf.keras.Input(shape=(None, None, 3))
 
     # Create Image detail
-    gaussian_output = GaussianFilter(5, 0, 1)(inputs)
-    image_detail = tf.keras.layers.Subtract()([inputs, gaussian_output])
-    detail_model = tf.keras.Model(inputs, image_detail)
+    low_freq_img = GaussianFilter(5, 0, 1)(inputs)
+    high_freq_img = tf.keras.layers.Subtract()([inputs, low_freq_img])
+    decomposition_model = tf.keras.Model(inputs, [low_freq_img, high_freq_img])
 
-    detail_outputs = detail_enhancement_network(detail_model.output)
-    luminance_outputs = luminance_enhancement_network(inputs)
+    detail_outputs = detail_enhancement_network(decomposition_model.output[1])
+    luminance_outputs = luminance_enhancement_network(decomposition_model.output[0])
     # x = tf.keras.layers.Add()([detail_outputs, luminance_outputs])
     luminance_outputs_clip = ClipLayer()(luminance_outputs)
     x = tf.keras.layers.Add()([detail_outputs, luminance_outputs_clip])
     outputs = whole_image_enhancement_network(x)
 
-    # Calculation detail loss
-    true_detail = detail_model(inputs_y)
-    detail_loss = L1Losses(name='detail')([detail_outputs, true_detail])
+    # Calculation luminance loss and detail loss
+    true_low_freq, true_high_freq = decomposition_model(inputs_y_true)
+
+    luminance_loss = MSELosses(name='luminance')([luminance_outputs, true_low_freq])
+    detail_loss = L1Losses(name='detail')([detail_outputs, true_high_freq])
 
     # Trainable False
-    detail_model.trainable = False
-    for layer in detail_model.layers:
+    decomposition_model.trainable = False
+    for layer in decomposition_model.layers:
         layer.trainable = False
 
-    model = tf.keras.Model(inputs=[inputs, inputs_y], outputs=[detail_loss, luminance_outputs, outputs])
+    model = tf.keras.Model(inputs=[inputs, inputs_y_true], outputs=[detail_loss, luminance_loss, outputs])
     model.compile(optimizer=tf.keras.optimizers.Adam(0.01),    # optimizer=tf.keras.optimizers.SGD(0.001, 0.9, 0.0001),
-                  loss=[pass_loss, mse_loss, dssim_loss],
+                  loss=[pass_loss, pass_loss, dssim_loss],
                   loss_weights=[1, 1, 1])
 
     outputs_final = ClipLayer()(outputs)
